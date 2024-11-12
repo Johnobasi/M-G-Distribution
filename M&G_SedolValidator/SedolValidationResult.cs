5869c24ba479ee6f0313740d07e93c35739841b2
JohnInyang <talk2johninyang@yahoo.com>
2024-11-12 09:15:49 +0000

    code push to repo

--- M&G_SedolValidator/SedolValidationResult.cs
+++ M&G_SedolValidator/SedolValidationResult.cs
@@ -3,11 +3,8 @@
     internal class SedolValidationResult(string inputString, bool isValidSedol, bool isUserDefined, string validationDetails) : ISedolValidationResult
     {
         public string InputString { get; } = inputString;
-
         public bool IsValidSedol { get; } = isValidSedol;
-
         public bool IsUserDefined { get; } = isUserDefined;
-
         public string ValidationDetails { get; } = validationDetails;
     }
 }
